ee20f280b6bff56a2597b7bc7c93d7feafbee0b4
radioactive1337 <vysotskiidanil@gmail.com>
2024-06-08 00:20:29 +0600

    add test context to tests

--- src/test/java/autotests/api/controller/DuckActionController/SwimTests.java
+++ src/test/java/autotests/api/controller/DuckActionController/SwimTests.java
@@ -6,10 +6,9 @@ import autotests.payloads.WingsState;
 import com.consol.citrus.TestCaseRunner;
 import com.consol.citrus.annotations.CitrusResource;
 import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.context.TestContext;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class SwimTests extends DuckActionsClient {
     @Test(description = "Проверка swim с существующим id")
@@ -29,7 +28,7 @@ public class SwimTests extends DuckActionsClient {
 
     @Test(description = "Проверка swim с несуществующим id")
     @CitrusTest
-    public void swimNotExistedDuckTest(@Optional @CitrusResource TestCaseRunner runner) {
+    public void swimNotExistedDuckTest(@Optional @CitrusResource TestCaseRunner runner, @Optional @CitrusResource TestContext context) {
         //  создаем утку
         Duck duck = new Duck().color("green").height(1.1).material("rubber").sound("quack").wingsState(WingsState.ACTIVE);
         createDuck(runner, duck);
@@ -37,10 +36,9 @@ public class SwimTests extends DuckActionsClient {
         //  получаем id утки
         getDuckId(runner);
         //  создаем несуществующий id
-        AtomicInteger notExistedDuckId = new AtomicInteger(0);
-        run(testContext -> notExistedDuckId.set(Integer.parseInt(testContext.getVariable("duckId")) + 100));
+        int notExistedDuckId = Integer.parseInt(context.getVariable("duckId")) + 1;
         //  плаваем
-        swimDuck(runner, notExistedDuckId.toString());
+        swimDuck(runner, String.valueOf(notExistedDuckId));
         //  проверяем ответ
         String expectedString = "{\n" +
                 "  \"timestamp\": \"@ignore()@\",\n" +
--- src/test/java/autotests/api/controller/DuckController/UpdateDuckTests.java
+++ src/test/java/autotests/api/controller/DuckController/UpdateDuckTests.java
@@ -5,30 +5,28 @@ import autotests.payloads.WingsState;
 import com.consol.citrus.TestCaseRunner;
 import com.consol.citrus.annotations.CitrusResource;
 import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.context.TestContext;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
 import autotests.clients.DuckActionsClient;
 import autotests.payloads.Message;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class UpdateDuckTests extends DuckActionsClient {
 
     @Test(description = "Проверка обновления цвета и высоты утки")
     @CitrusTest
-    public void updateColorAndHeightDuckTest(@Optional @CitrusResource TestCaseRunner runner) {
+    public void updateColorAndHeightDuckTest(@Optional @CitrusResource TestCaseRunner runner, @Optional @CitrusResource TestContext context) {
         //  создаем утку
         Duck duck = new Duck().color("green").height(1.1).material("rubber").sound("quack").wingsState(WingsState.ACTIVE);
         createDuck(runner, duck);
 
         //  получаем id созданной утки
         getDuckId(runner);
-        AtomicInteger duckid = new AtomicInteger(0);
-        run(testContext -> duckid.set(Integer.parseInt(testContext.getVariable("duckId"))));
+        int duckid = Integer.parseInt(context.getVariable("duckId"));
         //  обновляем 2 поля
         updateDuck(runner, "rainbow", "9.9", "${duckId}", "rubber", "quack", "ACTIVE");
         // проверяем ответ
-        String expectedMessage = String.format("Duck with id = %d is updated", duckid.get());
+        String expectedMessage = String.format("Duck with id = %d is updated", duckid);
         validateResponseByString(runner, 200, "{\n" +
                 "  \"message\": \"" + expectedMessage + "\"\n" +
                 "}");
@@ -36,19 +34,18 @@ public class UpdateDuckTests extends DuckActionsClient {
 
     @Test(description = "Проверка обновления цвета и звука утки")
     @CitrusTest
-    public void updateColorAndSoundDuckTest(@Optional @CitrusResource TestCaseRunner runner) {
+    public void updateColorAndSoundDuckTest(@Optional @CitrusResource TestCaseRunner runner, @Optional @CitrusResource TestContext context) {
         //  создаем утку
         Duck duck = new Duck().color("green").height(1.1).material("rubber").sound("quack").wingsState(WingsState.ACTIVE);
         createDuck(runner, duck);
 
         //  получаем id созданной утки
         getDuckId(runner);
-        AtomicInteger duckid = new AtomicInteger(0);
-        run(testContext -> duckid.set(Integer.parseInt(testContext.getVariable("duckId"))));
+        int duckid = Integer.parseInt(context.getVariable("duckId"));
         //  обновляем 2 поля
         updateDuck(runner, "green", "1.1", "${duckId}", "rubber", "mew", "ACTIVE");
         //  проверяем ответ
-        String expectedMessage = String.format("Duck with id = %d is updated", duckid.get());
+        String expectedMessage = String.format("Duck with id = %d is updated", duckid);
         validateResponseByClass(runner, 200, new Message().message(expectedMessage));
     }
 
